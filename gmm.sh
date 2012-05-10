03ce7e53cda5f6f561749bfde6ebf63158010443
Nick Groenen <zoni@zoni.nl>
2012-05-10 21:49:19 +0200

    Add argument parsing using getopt

--- gmm.sh
+++ gmm.sh
@@ -3,7 +3,6 @@
 # Git Multi Merge (gmm) - Easily merge one git branch into one or more other branches
 # Author: Nick Groenen
 # Email: zoni@zoni.nl
-# Version: 1.0
 # License: Public domain
 
 # {{{ Colored output
@@ -24,14 +23,59 @@ reset='\e[1;0m'
 # $2 = message
 cecho()
 {
-	color=${1:-""} # Default to no color, if not specified.
-	printf "${color}"
-	echo "${2}"
-	printf "${reset}"
+	if [ $COLOR -eq 0 ]
+	then
+		echo "${2}"
+	else
+		color=${1:-""} # Default to no color, if not specified.
+		printf "${color}"
+		echo "${2}"
+		printf "${reset}"
+	fi
 }
 # }}}
 
 # {{{ Argument parsing 
+
+# Defaults and such
+PROGRAM_NAME="gmm"
+FETCH=0
+MERGE_ORIGIN=0
+COLOR=1
+
+# Parse arguments
+OPTS=$(getopt -n $PROGRAM_NAME --options fm --longoptions fetch,merge-origin,color,no-color -- "$@")
+[ $? = 0 ] || { cecho ${red} "Error parsing arguments. Try $PROGRAM_NAME --help" ; exit 1; }
+
+eval set -- "$OPTS"
+while true; do
+	case $1 in
+	-f|--fetch)
+		FETCH=1;
+		shift; continue
+	;;
+	-m|--merge-origin)
+		MERGE_ORIGIN=1;
+		shift; continue
+	;;
+	--color)
+		COLOR=1;
+		shift; continue
+	;;
+	--no-color)
+		COLOR=0;
+		shift; continue
+	;;
+	--)
+		# No more arguments to parse
+		shift; break
+	;;
+	*)
+		cecho ${red} "Error: Unknown option $1"
+		exit 1
+	;;
+	esac
+done
 
 if [ $# -lt 2 ]
 then
@@ -55,35 +99,23 @@ git rev-parse > /dev/null 2>&1 || { cecho ${red} "Error: This isn't a valid git 
 # Store the current branch so we can switch back to it later
 STARTBRANCH=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3)
 
-cecho ${green} "Fetching upstream repository data (git fetch)"
-git fetch
-if [ $? -ne 0 ]
+# Fetch from upstream if requested
+if [ $FETCH -eq 1 ]
 then
-	cecho ${red} "Error: git fetch failed"
-	while true
-	do
-		echo -n "Continue anyway? [Yes/No] "
-		read answer
-		case "${answer}" in 
-			"Y"|"y"|"Yes"|"yes" )
-				break
-                ;;
-			"N"|"n"|"No"|"no" )
-                exit 1
-                ;;
-			*)
-				echo "Please answer yes or no"
-				;;
-		esac
-	done
+	cecho ${green} "Fetching upstream repository data (git fetch)"
+	git fetch || { cecho ${red} "Error: git fetch failed"; exit 1; }
 fi
 
+# Merge the branches
 for BRANCH in ${DESTINATIONBRANCHES}
 do
 	cecho ${green} "Switching to branch ${BRANCH}"
 	git checkout ${BRANCH} || { cecho ${red} "Error: Failed to switch to branch ${BRANCH}"; exit 1; }
-	cecho ${green} "Merging origin/${BRANCH} into ${BRANCH} to ensure your local branch is fully up-to-date"
-	git merge origin/${BRANCH} || { cecho ${red} "Error: Merge failed"; exit 1; }
+	if [ $MERGE_ORIGIN -eq 1 ]
+	then
+		cecho ${green} "Merging origin/${BRANCH} into ${BRANCH}"
+		git merge origin/${BRANCH} || { cecho ${red} "Error: Merge failed"; exit 1; }
+	fi
 	cecho ${magenta} "Merging master into ${BRANCH}"
 	git merge master || { cecho ${red} "Error: Merge failed"; exit 1; }
 done
